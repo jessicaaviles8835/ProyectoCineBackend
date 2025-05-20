08ebe4101e217ca83f853cb4c0a16906fa1ac2be
jessica <45468048+jessicaaviles8835@users.noreply.github.com>
2025-05-19 22:08:35 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ const MYSQLPASSWORD = process.env.MYSQLPASSWORD||'Jessica$1';
 const MYSQLDATABASE = process.env.MYSQLDATABASE||'cine_lotus_db';
 const MYSQLDATABASETEST = process.env.MYSQLDATABASETEST||'cine_lotus_testdb';
 const MYSQLPORT = process.env.MYSQLPORT||3306;
-const MYSQLURL = process.env.MYSQL_URL||'mysql://jessica:Jessica$1@localhost:3306/cine_lotus_db';
+const MYSQLURL = process.env.MYSQLURL||'mysql://jessica:Jessica$1@localhost:3306/cine_lotus_db';
 
 module.exports = {MYSQLHOST,MYSQLUSER,MYSQLPASSWORD,MYSQLDATABASE,MYSQLDATABASETEST,MYSQLPORT,MYSQLURL};
