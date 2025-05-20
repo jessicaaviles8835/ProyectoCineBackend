0b33221ace89577157f42ddf7a69172a2df805c3
jessica <45468048+jessicaaviles8835@users.noreply.github.com>
2025-05-19 21:44:37 -0500

    Prueba con URI

--- db.js
+++ db.js
@@ -1,31 +1,6 @@
 const mysql = require('mysql2');
-const { MYSQLHOST, MYSQLUSER, MYSQLPASSWORD,MYSQLDATABASE,MYSQLDATABASETEST,MYSQLPORT } = require('./config');
+const { MYSQLURL } = require('./config');
 
-const config = {
-  development: {
-    host: MYSQLHOST,
-    port:MYSQLPORT,
-    user: MYSQLUSER,
-    password: MYSQLPASSWORD,
-    database: MYSQLDATABASE, // base real
-    waitForConnections:true, //Espera si todas las conexiones están en uso
-    connectionLimit: 100, //Número máximo de conexiones simultáneas
-    queueLimit:0 //Número de consultas en espera (0 significa sin límite)
-  },
-  test: {
-    host: MYSQLHOST,
-    port:MYSQLPORT,
-    user: MYSQLUSER,
-    password: MYSQLPASSWORD,
-    database: MYSQLDATABASETEST, // base pruebas
-    waitForConnections:true, //Espera si todas las conexiones están en uso
-    connectionLimit: 100, //Número máximo de conexiones simultáneas
-    queueLimit:0 //Número de consultas en espera (0 significa sin límite)
-  }
-};
-
-const env = process.env.NODE_ENV || 'development';
-
-const pool = mysql.createPool(config[env]);
+const pool = mysql.createPool(MYSQLURL);
 
 module.exports = pool;
