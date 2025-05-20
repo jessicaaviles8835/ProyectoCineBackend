fcb966f51153c43a1724082ebc6e1eb0066fc3f6
jessica <45468048+jessicaaviles8835@users.noreply.github.com>
2025-05-19 21:22:19 -0500

    cambiando la config

--- db.js
+++ db.js
@@ -1,23 +1,23 @@
 const mysql = require('mysql2');
-const { DB_HOST, DB_USER, DB_PORT,DB_PASSWORD,DB_NAME,DB_NAME_TEST } = require('./config');
+const { MYSQLHOST, MYSQLUSER, MYSQLPASSWORD,MYSQLDATABASE,MYSQLDATABASETEST,MYSQLPORT } = require('./config');
 
 const config = {
   development: {
-    host: DB_HOST,
-    port:DB_PORT,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME, // base real
+    host: MYSQLHOST,
+    port:MYSQLPORT,
+    user: MYSQLUSER,
+    password: MYSQLPASSWORD,
+    database: MYSQLDATABASE, // base real
     waitForConnections:true, //Espera si todas las conexiones están en uso
     connectionLimit: 100, //Número máximo de conexiones simultáneas
     queueLimit:0 //Número de consultas en espera (0 significa sin límite)
   },
   test: {
-    host: DB_HOST,
-    port:DB_PORT,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME_TEST, // base pruebas
+    host: MYSQLHOST,
+    port:MYSQLPORT,
+    user: MYSQLUSER,
+    password: MYSQLPASSWORD,
+    database: MYSQLDATABASETEST, // base pruebas
     waitForConnections:true, //Espera si todas las conexiones están en uso
     connectionLimit: 100, //Número máximo de conexiones simultáneas
     queueLimit:0 //Número de consultas en espera (0 significa sin límite)
